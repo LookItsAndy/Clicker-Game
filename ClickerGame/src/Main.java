74fd73b3451780853a73be90de9e800b72ae666c
LookItsAndy <59989200+LookItsAndy@users.noreply.github.com>
2020-12-23 19:11:01 -0800

    Deleted JLabel for upgrader
    Made a required amount to buy power and setup GridLayout

--- ClickerGame/src/Main.java
+++ ClickerGame/src/Main.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Font;
+import java.awt.GridLayout;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -14,10 +15,14 @@ public class Main {
     //int for buttons
     int money;
     int power = 1;
+    int powerCost = 10;
 
     //labels
     JLabel balance;
-    JLabel powerLevel;
+
+    //buttons
+    JButton mainButton;
+    JButton powerUpgrade;
 
     //fonts
     Font font1;
@@ -42,53 +47,58 @@ public class Main {
     public void Font() {
         
         font1 = new Font("Comic Sans MS", Font.PLAIN, 32);
-        upgradesFont = new Font("Comic Sans MS", Font.PLAIN, 20);
+        upgradesFont = new Font("Comic Sans MS", Font.PLAIN, 18);
         
 
     }
 
     public void GUI() {
+        //window
         JFrame frame = new JFrame();
         frame.setSize(700,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setBackground(Color.darkGray);
         frame.setLayout(null);
 
+        //panel main button
         JPanel mainPanel = new JPanel();
         mainPanel.setBounds(100, 150, 150, 200);
         mainPanel.setBackground(Color.darkGray);
         frame.add(mainPanel);
 
+        //panel for balance status
         JPanel topPanel = new JPanel();
         topPanel.setBounds(50, 100, 250, 50);
         topPanel.setBackground(Color.darkGray);
         frame.add(topPanel);
 
+        //label for balance status
         balance = new JLabel("Balance: 0");
         balance.setFont(font1);
         balance.setForeground(Color.white);
         topPanel.add(balance);
 
-        JButton mainButton = new JButton();
+        //main clicker button
+        mainButton = new JButton();
         mainButton.setFocusPainted(false);
         mainButton.setBorder(null);
         mainButton.setIcon(new ImageIcon("C:\\Users\\lyand\\OneDrive\\Pictures\\Saved Pictures\\uwu.png"));
         mainButton.addActionListener(cHandler);
         mainPanel.add(mainButton);
 
-        JPanel rightPanel = new JPanel();
-        rightPanel.setBounds(500, 50, 150, 350);
-        rightPanel.setBackground(Color.blue);
-        frame.add(rightPanel);
+        //shop panel 
+        JPanel shopPanel = new JPanel();
+        shopPanel.setBounds(500, 50, 150, 350);
+        shopPanel.setBackground(Color.blue);
+        shopPanel.setLayout(new GridLayout(6,1));
+        frame.add(shopPanel);
 
-        JButton pwrUpgrade = new JButton();
-        //pwrUpgrade.setBorder(null);
-        pwrUpgrade.addActionListener(pUpgrader);
-        rightPanel.add(pwrUpgrade);
-
-        powerLevel = new JLabel("Power" + "(" + power + ")");
-        powerLevel.setFont(upgradesFont);
-        pwrUpgrade.add(powerLevel);
+        //button to upgrade power per click
+        powerUpgrade = new JButton("Power" + "(" + power + ")");
+        powerUpgrade.setFocusPainted(false);
+        powerUpgrade.setFont(upgradesFont);
+        powerUpgrade.addActionListener(pUpgrader);
+        shopPanel.add(powerUpgrade);
 
         frame.setVisible(true);
     }
@@ -96,7 +106,11 @@ public class Main {
     public class ClickHandler implements ActionListener {
         public void actionPerformed(ActionEvent event) {
 
-    
+            String action = event.getActionCommand();
+
+            switch(action) {
+
+            }
             money += power;
             
             balance.setText("Balance: " + money);
@@ -107,9 +121,15 @@ public class Main {
 
     public class PowerUpgrader implements ActionListener {
         public void actionPerformed(ActionEvent event) {
-            power++;
+
+            if (powerCost <= money) {
+                power++;
+                money -= powerCost;
+                balance.setText("Balance: " + money);
+            } 
+
             
-            powerLevel.setText("Power" + "(" + power + ")");
+            powerUpgrade.setText("Power" + "(" + power + ")");
             
 
         }
